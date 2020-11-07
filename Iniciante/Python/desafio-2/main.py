75c87474c72bec62d865b98ff09d2293a555f65c
Pedro Henrique Garcia Schuch <PedRo-HenRique-14@users.noreply.github.com>
2020-11-07 16:59:31 -0300

    Edição do main.py
    Edição do main do desafio 02

---
+++ Iniciante/Python/desafio-2/main.py
@@ -0,0 +1,11 @@
+'''
+Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X
+#1001
+'''
+
+a = float(input("Digite um número: "))
+b = float(input("Digite um número: "))
+
+x = a + b
+
+print(x)
